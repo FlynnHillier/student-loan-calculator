7e19e33408e775eb5ab2507b7b3db7c5a4a0a95f
FlynnHillier <flynnhillier@gmail.com>
2023-05-05 21:15:35 +0100

    added repayment functionality to StudentLoan; extrapolated types to seperate file;

--- StudentLoan.ts
+++ StudentLoan.ts
@@ -1,15 +1,9 @@
-interface YearsValue {
-    year:number
-    value:number
-}
-
-type Multiplier = number
-type Year = number
+import { Multiplier,Year,YearsValue } from "./types"
 
 export class StudentLoan {
-    private amount :number
-    private interest:Multiplier
-    private startYear:Year
+    public amount :number
+    public interest:Multiplier
+    public startYear:Year
 
     constructor(amount:number,interest:Multiplier,startYear:Year){
         this.amount = amount
@@ -20,9 +14,10 @@ export class StudentLoan {
     /**
      * 
      * @param period - the period in years in which loan values should be calculated
+     * @param repayment - {absolute}: an absolute value to be deducted from the value of the loan annually; {percentage}: a percentage value to be repaid annually; #NOT ADVISABLE TO DECLARE BOTH 
      * @returns - {year,value}[] - year in question; value, value of loan at given year 
      */
-    getYearsPeriod(period:{start?:Year,end?:Year} = {}) : YearsValue[]{
+    getYearsPeriod(period:{start?:Year,end?:Year} = {},repayment:{absolute?:number,percentage?:Multiplier} = {}) : YearsValue[]{
         if(!period.start){
             period.start = this.startYear
         }
@@ -60,9 +55,16 @@ export class StudentLoan {
         //calculate further year's values
         let accumulator = calculations[0].value
         for(let i = 1; i < periodLength - zeroFill + 1; i++){
+            if(repayment){
+                accumulator -= repayment.absolute || 0
+                accumulator *= repayment.percentage || 1
+            }
+            
             accumulator *= this.interest
 
-
+            if(accumulator < 0){
+                accumulator = 0
+            }
 
             calculations[i] = {
                 year:period.start + zeroFill + i,
@@ -87,9 +89,10 @@ export class StudentLoan {
     /**
      * 
      * @param year - year to recieve data bout
+     * @param repayment - {absolute}: an absolute value to be deducted from the value of the loan annually; {percentage}: a percentage value to be repaid annually; #NOT ADVISABLE TO DECLARE BOTH
      * @returns - {year,value} - year in question; value, value of loan at given year 
      */
-    getSingleYear(year:Year) : YearsValue{
+    getSingleYear(year:Year,repayment:{absolute?:number,percentage?:Multiplier} = {}) : YearsValue{
         const yearsAfterStart = year - this.startYear
         if(yearsAfterStart < 0){
             return {
@@ -100,7 +103,15 @@ export class StudentLoan {
 
         let accumulator = this.amount
         for(let i= 1; i < yearsAfterStart; i++){
+            if(repayment){
+                accumulator -= repayment.absolute || 0
+                accumulator *= repayment.percentage || 1
+            }
             accumulator *= this.interest
+
+            if(accumulator < 0){
+                accumulator = 0
+            }
         }
         return {
             year:year,
---
+++ types.d.ts
@@ -0,0 +1,7 @@
+export type Multiplier = number
+export type Year = number
+
+export interface YearsValue {
+    year:number
+    value:number
+}
