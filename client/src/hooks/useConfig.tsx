a38fb52db62dcc0b719cb2be6b561f3bbc03814e
FlynnHillier <flynnhillier@gmail.com>
2023-07-07 13:54:35 +0100

    implemented InputPlus component; reworked config hook return; removed bootstrap css import

--- client/src/hooks/useConfig.tsx
+++ client/src/hooks/useConfig.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Multiplier,Year } from "../types/types"
 
 export const useConfig = () => {
     let [interest,setInterest] = useState<Multiplier>(1) //multiplier
     let [installments,setInstallments] = useState<Map<Year,number>>(new Map<Year,number>())
-
+    let [incomeAmount,setIncomeAmount] = useState<number>(0)
     
     return {
-        interest,
-        setInterest,
-        installments,
-        setInstallments,
+        interest:{
+            state:interest,
+            set:setInterest,
+        },
+        installments:{
+            state:installments,
+            set:setInstallments,
+        },
+        incomeAmount:{
+            state:incomeAmount,
+            set:setIncomeAmount,
+        }
     }
 }
